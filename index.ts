b38ef5725ecfe031d88b90210cb78b6d5863db6e
Sai Ashirwad <saiashirwadr@gmail.com>
2024-10-20 05:47:49 +0530

    hmm

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import type { LinkedList as L } from "./src/linked-list";
 
-type linkedListResult = L.pop<
-	L.push<L.push<L.push<L.push<[0], 5>, 5>, 6>, 7>
->;
+type result = L.insertAtFront<L.push<L.create<2>, 5>, 20>;
+
+// type linkedListResult = L.pop<
+// 	L.push<L.push<L.push<L.push<[0], 5>, 5>, 6>, 7>
+// >;
+//
+// type result = L.pop<L.push<[1, 2, 3], 5>>;
--- src/linked-list.ts
+++ src/linked-list.ts
@@ -1,19 +1,39 @@
 export namespace LinkedList {
-	export type push<li extends any[], value extends any> = [
-		...li,
-		value,
-	];
+	export type create<value> = { value: value; next: null };
 
-	export type pop<
-		li extends any[],
-		acc extends any[] = [],
-	> = li extends []
-		? [[], null]
-		: li extends [infer x]
-			? [[], x]
-			: li extends [infer x1, infer x2]
-				? [[...acc, x1], x2]
-				: li extends [infer x, ...infer xs]
-					? pop<xs, [x, ...acc]>
-					: never;
+	export type insertAtFront<
+		li extends { value: any; next: any },
+		value extends any,
+	> = {
+		value: value;
+		next: li;
+	};
+
+	export type push<
+		li extends { value: any; next: any },
+		value extends any,
+	> = li["next"] extends null
+		? {
+				value: li["value"];
+				next: { value: value; next: null };
+			}
+		: {};
+
+	// export type push<li extends any[], value extends any> = [
+	// 	...li,
+	// 	value,
+	// ];
+	//
+	// export type pop<
+	// 	li extends any[],
+	// 	acc extends any[] = [],
+	// > = li extends []
+	// 	? [[], null]
+	// 	: li extends [infer x]
+	// 		? [[], x]
+	// 		: li extends [infer x1, infer x2]
+	// 			? [[...acc, x1], x2]
+	// 			: li extends [infer x, ...infer xs]
+	// 				? pop<xs, [...acc, x]>
+	// 				: never;
 }
