ce0dfe92a322cb7bf299a2da54b8fffdc75e36fc
Sai Ashirwad <saiashirwadr@gmail.com>
2024-10-20 06:46:45 +0530

    so cursed

--- index.ts
+++ index.ts
@@ -1,9 +1,31 @@
 import type { LinkedList as L } from "./src/linked-list";
 
-type result = L.insertAtFront<L.push<L.create<2>, 5>, 20>;
+// type result = L.insertAtFront<L.push<L.create<2>, 5>, 20>;
 
 // type linkedListResult = L.pop<
 // 	L.push<L.push<L.push<L.push<[0], 5>, 5>, 6>, 7>
 // >;
 //
 // type result = L.pop<L.push<[1, 2, 3], 5>>;
+//
+
+type lol = {
+	value: 2;
+	next: {
+		value: 5;
+		next: {
+			value: 10;
+			next: {
+				value: 20;
+				next: null;
+			};
+		};
+	};
+};
+
+// type lolol = L.reverse<lol>;
+
+type result = L.toArray<lol>;
+// type li = L.fromArray<[2, 3, 4]>;
+
+// type r = L.pop<lol>;
--- src/linked-list.ts
+++ src/linked-list.ts
@@ -1,5 +1,40 @@
+type reverseArray<
+	li extends any[],
+	acc extends any[] = [],
+> = li extends [infer x, ...infer xs]
+	? reverseArray<xs, [x, ...acc]>
+	: li extends [infer x]
+		? [x]
+		: acc;
+
 export namespace LinkedList {
 	export type create<value> = { value: value; next: null };
+
+	export type toArray<
+		li extends { value: any; next: any },
+		acc extends any[] = [],
+	> = li["next"] extends null
+		? li["value"] extends null
+			? acc
+			: [...acc, li["value"]]
+		: toArray<li["next"], [...acc, li["value"]]>;
+
+	type _fromArray<
+		arr extends any[],
+		acc = null,
+	> = arr extends [infer x, ...infer xs]
+		? _fromArray<xs, { value: x; next: acc }>
+		: arr extends [infer x]
+			? { value: x; next: acc }
+			: acc;
+
+	export type fromArray<arr extends any[]> = _fromArray<
+		reverseArray<arr>
+	>;
+
+	export type reverse<
+		li extends { value: any; next: any },
+	> = LinkedList.fromArray<LinkedList.toArray<li>>;
 
 	export type insertAtFront<
 		li extends { value: any; next: any },
@@ -12,28 +47,27 @@ export namespace LinkedList {
 	export type push<
 		li extends { value: any; next: any },
 		value extends any,
-	> = li["next"] extends null
-		? {
-				value: li["value"];
-				next: { value: value; next: null };
-			}
-		: {};
+	> = li["value"] extends null
+		? "Invalid"
+		: li["next"] extends null
+			? {
+					value: li["value"];
+					next: { value: value; next: null };
+				}
+			: {}; // tbd
 
-	// export type push<li extends any[], value extends any> = [
-	// 	...li,
-	// 	value,
-	// ];
-	//
-	// export type pop<
-	// 	li extends any[],
-	// 	acc extends any[] = [],
-	// > = li extends []
-	// 	? [[], null]
-	// 	: li extends [infer x]
-	// 		? [[], x]
-	// 		: li extends [infer x1, infer x2]
-	// 			? [[...acc, x1], x2]
-	// 			: li extends [infer x, ...infer xs]
-	// 				? pop<xs, [...acc, x]>
-	// 				: never;
+	export type pop<
+		li extends { value: any; next: any },
+		acc = {},
+	> = li["value"] extends null
+		? "Can't pop from empty list"
+		: li["next"] extends null
+			? [acc, li["value"]]
+			: pop<
+					{
+						value: li["next"]["value"];
+						next: li["next"]["next"];
+					},
+					{ value: li["value"]; next: acc }
+				>;
 }
