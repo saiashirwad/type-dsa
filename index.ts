03a5c207d77751b835cb8e564bf22b7680b7cd39
Sai Ashirwad <saiashirwadr@gmail.com>
2024-10-20 11:22:40 +0530

    Hmm

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import type { BST } from "./src/bst";
-import type { Numbers } from "./src/numbers";
+import type { Stack } from "./src/stack";
 
 type tree = BST.fromArray<[5, 4, 10, 2, 20, 8]>;
+
 type result = BST.search<tree, 10>;
+
+type stack = Stack.fromArray<[1, 2, 3, 4, 5], 6>;
+type popResult = Stack.pop<stack>;
---
+++ src/stack.ts
@@ -0,0 +1,39 @@
+import type { tail } from "./utils";
+
+export namespace Stack {
+	export type create<value, capacity extends number> = {
+		data: [value];
+		capacity: capacity;
+	};
+
+	export type fromArray<
+		arr extends number[],
+		capacity extends number,
+		acc extends {
+			capacity: number;
+			data: number[];
+		} = {
+			capacity: capacity;
+			data: [];
+		},
+	> = arr["length"] extends 0
+		? acc
+		: fromArray<tail<arr>, capacity, push<acc, arr[0]>>;
+
+	export type push<
+		stack extends { data: any[]; capacity: number },
+		value extends number,
+	> = stack["data"]["length"] extends stack["capacity"]
+		? stack
+		: {
+				data: [value, ...stack["data"]];
+				capacity: stack["capacity"];
+			};
+
+	export type pop<
+		stack extends { data: any[]; capacity: number },
+	> = {
+		data: tail<stack["data"]>;
+		capacity: stack["capacity"];
+	};
+}
